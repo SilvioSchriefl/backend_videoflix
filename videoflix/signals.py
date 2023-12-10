a04e2cec29b1c4a60031d3a154c5dddc3d3986ea
Silvio Schriefl <125505936+SilvioSchriefl@users.noreply.github.com>
2023-12-10 20:57:15 +0100

    add thumbnail function

--- videoflix/signals.py
+++ videoflix/signals.py
@@ -1,15 +1,17 @@
-from videoflix.tasks import convert_480p_and_update_model
+from videoflix.tasks import convert_480p_and_update_model, create_thumbnail
 from .models import Video
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 import os 
 import django_rq
 
 
 @receiver(post_save, sender=Video)
-def video_post_save(sender, instance, created, **kwargs):
+def video_pre_save(sender, instance, **kwargs): 
+    created = kwargs.get('created', False)
     if created:
         convert_480p_and_update_model(instance.file.path, instance)
+        create_thumbnail(instance.file.path, instance)
         #queue = django_rq.get_queue('default', autocommit=True)
         #queue.enqueue(convert_480p, instance.file.path)
         #queue.enqueue(convert_720p, instance.file.path)
@@ -21,5 +23,3 @@ def video_post_delete(sender, instance, **kwargs):
     if instance.file:
         if os.path.exists(instance.file.path):
             os.remove(instance.file.path)
-            os.remove(instance.file_480p.path)
-            os.remove(instance.thumbnail.path)
--- videoflix/tasks.py
+++ videoflix/tasks.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+from .models import Thumbnail
 
 def convert_480p_and_update_model(source, video_instance):
     base_path, file_name = os.path.split(source)
@@ -15,7 +16,7 @@ def convert_480p_and_update_model(source, video_instance):
         '-crf', '23',
         '-c:a', 'aac',
         '-strict', '-2',
-        target
+       target,
     ]
 
     try:
@@ -29,3 +30,38 @@ def convert_480p_and_update_model(source, video_instance):
 
     except subprocess.CalledProcessError as e:
         print(f"Fehler bei der Konvertierung zu 480p: {e}")
+        
+def create_thumbnail(source, video_instance):
+    base_path, file_name = os.path.split(source)
+    file_name_without_extension, extension = os.path.splitext(file_name)
+
+    thumbnail_name = None  # Initialisiere die Variable hier
+
+    try:
+        thumbnail_name = f"{file_name_without_extension}_thumbnail.png"
+        thumbnail_path = os.path.join(base_path, thumbnail_name)
+
+        cmd = ['ffmpeg', '-i', source, '-ss', '00:00:05', '-vframes', '1', thumbnail_path]
+
+        subprocess.run(cmd, capture_output=True, check=True)
+        print(f"Thumbnail erfolgreich erstellt: {thumbnail_name}")
+
+        # Hier erstellen wir ein neues Thumbnail-Objekt
+        thumbnail = Thumbnail()
+        thumbnail.title = video_instance.title
+        thumbnail.category = "Default"  # Hier kannst du die Kategorie anpassen oder aus dem Video-Modell übernehmen
+        thumbnail.video = video_instance
+        thumbnail.thumbnail.save(thumbnail_name, open(thumbnail_path, 'rb'))
+
+        # Lösche das temporäre Thumbnail-File
+        os.remove(thumbnail_path)
+
+        print(f"Thumbnail erfolgreich im Modell Thumbnail gespeichert.")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Fehler bei der Thumbnail-Erstellung: {e}")
+
+        # Handle den Fehler hier weiter, wenn nötig
+
+    # Wenn es außerhalb des try-except-Blocks benötigt wird, kann thumbnail_name hier weiterverwendet werden.
+    return thumbnail_name
