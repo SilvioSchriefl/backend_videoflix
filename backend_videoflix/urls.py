d46461800919da8d1bb6040e2ebc6336826bce8d
Silvio Schriefl <125505936+SilvioSchriefl@users.noreply.github.com>
2023-12-10 22:08:10 +0100

    update urls

--- backend_videoflix/urls.py
+++ backend_videoflix/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path('django-rq/', include('django_rq.urls')),
 ] + staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
