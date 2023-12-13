a847ed55182fb9c0c916eba3eb1728fe92c1965e
Silvio Schriefl <125505936+SilvioSchriefl@users.noreply.github.com>
2023-12-13 20:43:36 +0100

    fix

--- videoflix/signals.py
+++ videoflix/signals.py
@@ -1,4 +1,4 @@
-from videoflix.tasks import convert_480p_and_update_model, create_thumbnail
+from videoflix.tasks import convert_480p_and_update_model
 from .models import Video
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete, pre_save
@@ -11,7 +11,6 @@ def video_pre_save(sender, instance, **kwargs):
     created = kwargs.get('created', False)
     if created:
         convert_480p_and_update_model(instance.file.path, instance)
-        create_thumbnail(instance.file.path, instance)
         #queue = django_rq.get_queue('default', autocommit=True)
         #queue.enqueue(convert_480p, instance.file.path)
         #queue.enqueue(convert_720p, instance.file.path)
