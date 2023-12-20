ce98f48d91eaa365ca412f9116fe831f839226e9
Silvio Schriefl <125505936+SilvioSchriefl@users.noreply.github.com>
2023-12-20 21:33:39 +0100

    fix

--- videoflix/models.py
+++ videoflix/models.py
@@ -29,7 +29,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     email_confirmed = models.BooleanField(default=False)
     user_name = models.CharField(max_length=100, default='', blank=False, null=False)
-    watchlist_video = models.JSONField(default=dict)
+    watchlist_video = models.JSONField(default=list)
     objects = CustomUserManager()
     USERNAME_FIELD = 'email'
     
